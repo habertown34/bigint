d9fbfc7f1dee1006542af306cdebed557a77704c
Maximilian Reichl <maxi.reichl@outlook.de>
2022-07-26 20:14:21 +0200

    Refreshed repository

--- src/main.c
+++ src/main.c
@@ -10,14 +10,14 @@ int main()
     BigInteger* p = &b;
     b.size = 1;
     b.data = (unsigned int*) malloc((b.size)* sizeof(unsigned int));
-    b.data[0] = 0b01111111111111111111111111111111;
+    b.data[0] = 0b10000000000000000000000000000000;
     //b.data[1] = 1;
     
     BigInteger b2;
     BigInteger* p2 = &b2;
     b2.size = 1;
     b2.data = (unsigned int*) malloc((b2.size)* sizeof(unsigned int));
-    b2.data[0] = 1;
+    b2.data[0] = -1;
     //b2.data[1] = 1;
     printBIData(p);
     printBIData(p2);    
